636df5a64522d0399aeee1a2d68cc046f8e01515
Yusuf Dauda <yusufdauda41@gmail.com>
2026-01-23 23:12:45 +0100

    fix: make PrismaService Railway-safe (lazy connection)

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,12 +1,26 @@
-import { Injectable, OnModuleInit } from '@nestjs/common';
+import { Injectable, OnModuleDestroy } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class PrismaService
   extends PrismaClient
-  implements OnModuleInit
+  implements OnModuleDestroy
 {
-  async onModuleInit() {
-    await this.$connect();
+  constructor() {
+    super({
+      log:
+        process.env.NODE_ENV === 'development'
+          ? ['query', 'error', 'warn']
+          : ['error'],
+    });
+  }
+
+  /**
+   * Prisma should NOT force a connection on startup.
+   * It will connect lazily when the first query runs.
+   */
+
+  async onModuleDestroy() {
+    await this.$disconnect();
   }
 }
